cd6a5ae00efebe1e86b6452d607f51d1516512b0
Max Calabrese <max.calabrese@ymail.com>
2015-03-01 15:46:05 +0100

    add method for creating generic html elements

--- src/TagHelper.php
+++ src/TagHelper.php
@@ -4,6 +4,11 @@ namespace Maxcal\TagHelper;
 
 use Symfony\Component\Templating\Helper\Helper;
 
+use \DOMDocument, \DOMElement;
+
+/**
+ *
+ */
 class TagHelper extends Helper {
 
     /**
@@ -17,4 +22,27 @@ class TagHelper extends Helper {
     {
         return 'tag';
     }
+
+
+    /**
+     * Low level method to create generic HTML elements
+     * @param $tag_name
+     * @param $value string | DOMElement - the node value of the created element
+     * @param $attibutes array - (optional) attributes to apply to the node
+     * @api
+     * @return string
+     */
+    public function createTag($tag_name, $value, $attributes = array()){
+        $doc = new DOMDocument();
+        $el = $doc->createElement($tag_name, $value);
+
+        foreach ($attributes as $key => $val) {
+            $el->setAttribute($key, $val);
+        }
+        $doc->appendChild($el);
+        return trim($doc->saveHTML());
+    }
+
+
+
 }
--- tests/TagHelperTest.php
+++ tests/TagHelperTest.php
@@ -14,8 +14,23 @@ class TagHelperTest extends \PHPUnit_Framework_TestCase {
     }
 
     public function test_getName(){
-        $this->assertEquals('tag', $this->subject->getName());
+        $this->assertEquals('tag', $this->subject->getName(),
+            'TagHelper should implement the getName function so that it can be used with the Symfony Templating Component'
+        );
+    }
+
+    public function test_createTag(){
+        $tag = $this->subject->createTag('p', 'Hello World!');
+        $this->assertEquals('<p>Hello World!</p>', $tag,
+            'it should create basic html elements'
+        );
+    }
+
+    public function test_createTag_adds_attributes(){
+        $tag = $this->subject->createTag('a', 'Hello World!', ['href' => '#']);
+        $this->assertEquals('<a href="#">Hello World!</a>', $tag,
+            'it should apply attributes supplied to the element'
+        );
     }
 
 }
- 
